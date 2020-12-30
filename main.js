533ce8b9af3bf778848ddc4e3d3f4b6048678e60
El122 <lazyelf472@gmail.com>
2020-12-30 23:10:31 +0500

    commit

--- main.js
+++ main.js
@@ -1,37 +1,163 @@
 const startButt = document.getElementById("startButt");
-const checkBox = document.getElementById("players")
+let gameOver = false;
+let players_arr = [];
 
 class Gameboard {
     constructor(board, players) {
-        let gameboard = board.getElementsByClassName("gameBoard")[0];
-
-        this.createBoard(gameboard);
+        this.gameboard = board.getElementsByClassName("gameBoard")[0];
+        this.gameboard.style.display = "grid";
+        this.field = this.createBoard(this.gameboard);
+        this.renderPlayersCards(players);
     }
 
     createBoard = (gameboard) => {
         gameboard.innerHTML = "";
         let squares = 9;
         for (let i = 0; i < squares; ++i) {
-            let boardSquare = document.createElement("div");
+            let boardSquare = document.createElement("button");
 
             boardSquare.classList.add("square");
+            boardSquare.setAttribute("player", "");
             gameboard.appendChild(boardSquare);
         }
+
+        return (gameboard.getElementsByClassName("square"));
+    }
+
+    renderPlayersCards = (players) => {
+        players.player1.createCard(0);
+        players.player2.createCard(1);
+    }
+
+    doStep = (field, sign, signImg) => {
+        if (field.getAttribute("player") != "") {
+            return false;
+        }
+
+        field.style.backgroundColor = "white";
+        field.style.backgroundImage = "URL(" + signImg + ")";
+        field.setAttribute("player", sign);
+
+        return true;
+    }
+
+    checkStep = (playerStep) => {
+        if (playerStep == "img/cake.svg") {
+            return ["img/cupcake.svg", "0"];
+        } else {
+            return ["img/cake.svg", "X"];
+        }
+    }
+
+    checkWinners = (gameBoard) => {
+        if (
+            (gameBoard[0].getAttribute("player") == gameBoard[1].getAttribute("player") && gameBoard[1].getAttribute("player") == gameBoard[2].getAttribute("player") ||
+                gameBoard[0].getAttribute("player") == gameBoard[3].getAttribute("player") && gameBoard[3].getAttribute("player") == gameBoard[6].getAttribute("player") ||
+                gameBoard[0].getAttribute("player") == gameBoard[4].getAttribute("player") && gameBoard[4].getAttribute("player") == gameBoard[8].getAttribute("player")) && gameBoard[0].getAttribute("player") != "" ||
+
+            (gameBoard[3].getAttribute("player") == gameBoard[4].getAttribute("player") && gameBoard[4].getAttribute("player") == gameBoard[5].getAttribute("player") ||
+                gameBoard[1].getAttribute("player") == gameBoard[4].getAttribute("player") && gameBoard[4].getAttribute("player") == gameBoard[7].getAttribute("player") ||
+                gameBoard[2].getAttribute("player") == gameBoard[4].getAttribute("player") && gameBoard[4].getAttribute("player") == gameBoard[6].getAttribute("player")) && gameBoard[4].getAttribute("player") != "" ||
+
+            (gameBoard[6].getAttribute("player") == gameBoard[7].getAttribute("player") && gameBoard[7].getAttribute("player") == gameBoard[8].getAttribute("player") ||
+                gameBoard[2].getAttribute("player") == gameBoard[5].getAttribute("player") && gameBoard[5].getAttribute("player") == gameBoard[8].getAttribute("player")) && gameBoard[8].getAttribute("player") != ""
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    gameOver = (players, sign) => {
+        let player1 = players.player1;
+        let player2 = players.player2;
+        let winner;
+
+        if (player1.sign == sign) {
+            winner = player1.name;
+            ++player1.wins;
+        } else {
+            winner = player2.name;
+            ++player2.wins;
+        }
+
+        this.gameboard.style.display = "block";
+        this.gameboard.innerHTML = winner + " is winner!!";
+
+        player1.createCard(0);
+        player1.createCard(1);
     }
 }
 
 class Player {
-    constructor(name) {
+    constructor(name, sign) {
         this.name = name;
-        // this.sign = sign;
+        this.sign = sign;
+        this.wins = 0;
     }
+
+    createCard = (playerNum) => {
+        const firstPlayerCard = document.getElementsByClassName("playerCard")[playerNum];
+        firstPlayerCard.getElementsByTagName("h2")[0].innerHTML = this.name;
+        firstPlayerCard.getElementsByTagName("p")[0].innerHTML = "WINS: " + this.wins;
+    }
+}
+function checkPlayersArr(playerName) {
+    players_arr.forEach(player => {
+        if (player == playerName) return player;
+    });
+
+    return false;
+}
+
+// function addPlayer() {
+
+// }
+
+function createPlayers(players) {
+    let playerName;
+    let player1;
+    let player2;
+
+    if (players == "AI") {
+        playerName = document.getElementsByClassName("firstPlayer")[0].value || "Some Unicorn";
+
+        if (!checkPlayersArr(playerName)) {
+            players_arr.push(new Player(playerName, "X"));
+        }
+
+        player1 = checkPlayersArr(playerName);
+
+        playerName = "AI";
+
+        if (!checkPlayersArr(playerName)) {
+            players_arr.push(new Player(playerName, "0"));
+        }
+
+        player2 = checkPlayersArr(playerName);
+    } else {
+        playerName = document.getElementsByClassName("firstPlayer")[0].value || "Some Unicorn";
+        if (!checkPlayersArr(playerName)) {
+            players_arr.push(new Player(playerName, "X"));
+        }
+        player1 = checkPlayersArr(playerName) || new Player(playerName, "X");
+
+        playerName = document.getElementsByClassName("secondPlayer")[0].value || "Maybe Unicorn";
+        if (!checkPlayersArr(playerName)) {
+            players_arr.push(new Player(playerName, "0"));
+        }
+        player2 = checkPlayersArr(playerName) || new Player(playerName, "0");
+    }
+
+    return { player1, player2 };
 }
 
 renderPlayersForm = (playersForm, players) => {
-    playersForm.innerHTML = "";
     let input = document.createElement("input");
+    let p = document.createElement("p");
+
+    playersForm.innerHTML = "";
+
     input.classList.add("firstPlayer");
-    let p = document.createElement("p")
     input.type = "text";
 
     if (players == "AI") {
@@ -50,54 +176,44 @@ renderPlayersForm = (playersForm, players) => {
     }
 }
 
-function checkPlayers() {
-    let playersForm = document.getElementsByClassName("players")[0];
-    let players = checkBox.checked ? "AI" : "players";
-
-    renderPlayersForm(playersForm, players);
-
-    return players;
-}
-
-function createPlayers(players) {
-    let playerName;
-    let player1;
-    let player2;
-
-    if (players == "AI") {
-        playerName = document.getElementsByClassName("firstPlayer")[0].value || "Some Human";
-        player1 = new Player(playerName);
-
-        playerName = "AI";
-        player2 = new Player(playerName);
-    }
-    // else {
-    //     playerName = document.getElementsByClassName("firstPlayer")[0].value || "Some Human";
-    //     player1 = new Player(playerName);
-
-    //     playerName = document.getElementsByClassName("secondPlayer")[0].value || "Maybe Human";
-    //     player2 = new Player(playerName);
-    // }
-
-    console.log(player1 + " " + player2);
-
-    // if (player1 != "" && player2 != "") {
-    //     return { player1, player2 };
-    // }
-
-    // return createPlayers(players);
+function checkPlayers(checkBox) {
+    return checkBox.checked ? "AI" : "players";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    let players = checkPlayers();
+    const checkBox = document.getElementById("players")
+
+    let playersForm = document.getElementsByClassName("players")[0];
+    let checkedPlayers = checkPlayers(checkBox); // проверка второго игрока
+    let playerStep = "img/cake.svg";
+    let sign = "X";
+    let players;
+    let gameBoard;
+
+    renderPlayersForm(playersForm, checkedPlayers);
 
     checkBox.onclick = () => {
-        players = checkPlayers();
+        checkedPlayers = checkPlayers(checkBox);
+        renderPlayersForm(playersForm, checkedPlayers);
     }
 
     startButt.onclick = () => {
         let board = document.getElementsByTagName("main")[0];
-        players = createPlayers(players);
-        let gameBoard = new Gameboard(board, players);
+
+        players = createPlayers(checkedPlayers);
+        gameBoard = new Gameboard(board, players);
+
+        for (let field of gameBoard.field) {
+            field.onclick = () => {
+                let step = gameBoard.doStep(field, sign, playerStep);
+                gameOver = gameBoard.checkWinners(gameBoard.field);
+
+                if (gameOver) gameBoard.gameOver(players, sign);
+
+                if (step) {
+                    [playerStep, sign] = gameBoard.checkStep(playerStep);
+                }
+            }
+        }
     }
-})
+});
